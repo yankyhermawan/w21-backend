227510897df6f7dabc3e24b5dc108ec96819a9b4
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-11 21:36:26 +0700

    Refactor customer service and customer guard imports

--- customer/customer.service.js
+++ customer/customer.service.js
@@ -1,10 +1,10 @@
-import { PrismaService } from "../prisma.service";
+import { PrismaService } from "../prisma.service.js";
 
 export class CustomerService {
 	constructor() {
 		this.prismaService = new PrismaService();
 	}
-
+	// without id as params
 	async get() {
 		const response = await this.prismaService.customer.findMany();
 		if (response) {
@@ -15,5 +15,48 @@ export class CustomerService {
 				},
 			};
 		}
+	}
+
+	async create(data) {
+		const response = await this.prismaService.customer.create({
+			data: data,
+		});
+		if (response) {
+			return {
+				code: 201,
+				message: {
+					response: response,
+				},
+			};
+		}
+		return {
+			code: 500,
+			message: {
+				response: "Internal Server Error",
+			},
+		};
+	}
+	// with id as params
+
+	async getById(id) {
+		const response = await this.prismaService.customer.findUnique({
+			where: {
+				id: id,
+			},
+		});
+		if (response) {
+			return {
+				code: 201,
+				message: {
+					response: response,
+				},
+			};
+		}
+		return {
+			code: 500,
+			message: {
+				response: "Internal Server Error",
+			},
+		};
 	}
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { CustomerGuard } from "./customer/customer.guard";
-import { CustomerService } from "./customer/customer.service";
+import { CustomerGuard } from "./customer/customer.guard.js";
+import { CustomerService } from "./customer/customer.service.js";
 
 const customerGuard = new CustomerGuard();
 const customerService = new CustomerService();
@@ -11,12 +11,43 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.route("/customer").get(async (req, res) => {
-	const token = String(req.headers["authorization"].split(" ")[1]);
-	const checkToken = customerGuard.checkTokenValid(token);
-	if (checkToken) {
-		const data = await customerService.get();
-		res.status(data.code).json(data.message.response);
+app
+	.route("/customer")
+	.get(async (req, res) => {
+		try {
+			const token = String(req.headers["authorization"].split(" ")[1]);
+			const checkToken = customerGuard.checkTokenValid(token);
+			if (checkToken) {
+				const data = await customerService.get();
+				res.status(data.code).json(data.message);
+			}
+		} catch (err) {
+			res.status(401).json({ response: "Invalid Token" });
+		}
+	})
+	.post(async (req, res) => {
+		try {
+			const token = String(req.headers["authorization"].split(" ")[1]);
+			const checkToken = customerGuard.checkTokenValid(token);
+			if (checkToken) {
+				const data = await customerService.create(req.body);
+				res.status(data.code).json(data.message);
+			}
+		} catch (err) {
+			res.status(401).json({ response: "Invalid Token" });
+		}
+	});
+
+app.route("/customer/:id").get(async (req, res) => {
+	try {
+		const token = String(req.headers["authorization"].split(" ")[1]);
+		const checkToken = customerGuard.checkTokenValid(token);
+		if (checkToken) {
+			const data = await customerService.getById(+req.params.id);
+			res.status(data.code).json(data.message);
+		}
+	} catch (err) {
+		res.status(401).json({ response: "Invalid Token" });
 	}
 });
 
